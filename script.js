556e6f728a595847bf188e0196f3ee66d12144ff
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-03-03 21:04:01 +0100

    feat: add new task (#2)

---
+++ script.js
@@ -0,0 +1,14 @@
+const addBtn = document.getElementById("btn-add");
+const clearBtn = document.getElementById("btn-clear");
+const delBtn = document.getElementById("material-icons");
+const tasks = document.getElementById("tasks");
+const input = document.getElementById("input");
+
+
+addBtn.addEventListener("click", () => {
+    tasks.innerHTML += `<div class="task-container">
+        <input type="checkbox" class="chbox">
+        <span class="task">${input.value}</span>
+        <button class="material-icons">&#xe872;</button></div>`;
+    input.value = "";
+});
