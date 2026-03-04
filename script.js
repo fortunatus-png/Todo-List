223c033499bc846c641f5443b43eca087f95e861
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-03-04 15:54:20 +0100

    refactor: improve add task logic

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const addBtn = document.getElementById("btn-add");
 const clearBtn = document.getElementById("btn-clear");
-const delBtn = document.getElementById("material-icons");
 const tasks = document.getElementById("tasks");
 const input = document.getElementById("input");
 
 
 addBtn.addEventListener("click", () => {
-    if(input.value !== "") {
-        tasks.innerHTML += `<div class="task-container">
+    if(input.value.trim() !== "") {
+        let newTask = document.createElement("div");
+        newTask.classList.add("task-container");
+        newTask.innerHTML = `
             <input type="checkbox" class="chbox">
             <span class="task">${input.value}</span>
-            <button class="material-icons">&#xe872;</button></div>`;
+            <button class="material-icons">&#xe872;</button>`;
+        tasks.appendChild(newTask);
         input.value = "";
     }
 });
