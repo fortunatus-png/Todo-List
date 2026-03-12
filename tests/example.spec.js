9383c3f717b4abf4dfbb18b5830f69d50d6ca706
Fortunatus <tumar.chegebaeva@informatik.hs-fulda.de>
2026-03-12 13:14:03 +0100

    Update test URLs to use Playwright site

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,15 +1,14 @@
-// @ts-check
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5501/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/To-Do's/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5501/');
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
