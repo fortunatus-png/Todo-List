e9692a5db188b448a4daa0febe32cae6762ff448
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-03-12 16:05:09 +0100

    update task handler to toggle or remove tasks

--- script.js
+++ script.js
@@ -17,12 +17,17 @@ const addTask = () => {
     }
 };
 
-const toggleTaskDone = e => {
+const toggleTaskDoneOrRemove = e => {
     const target = e.target;
-    if(target.type !== "checkbox") return;
+    const taskContainer = target.closest(".task-container");
+    if(!taskContainer) return;
+    const task = taskContainer.querySelector(".task");
     
-    const task = target.closest(".task");
-    task.classList.toggle("done", target.checked);
+    if(target.type === "checkbox") {
+        task.classList.toggle("done", target.checked);
+    }else if(target.classList.contains("deleteBtn")) {
+        taskContainer.remove();
+    }
 };
 
 const createCheckBox = () => {
@@ -41,8 +46,8 @@ const createSpanWithValue = () => {
 
 const createButtonWithIcon = () => {
     const btnEl = document.createElement("button");
-    btnEl.classList.add("material-icons");
     btnEl.setAttribute("type", "button");
+    btnEl.classList.add("deleteBtn", "material-icons");
     btnEl.innerHTML = "&#xe872;";
     return btnEl;
 };
@@ -50,22 +55,5 @@ const createButtonWithIcon = () => {
 const clearInput = () => input.value = "";
 
 addBtn.addEventListener("click", addTask);
-tasks.addEventListener("click", toggleTaskDone);
-    const taskContainer = target.closest(".task-container");
-    if(!taskContainer) return;
-    const task = taskContainer.querySelector(".task");
-    const chbox = taskContainer.querySelector(".chbox");
-    const btn = taskContainer.querySelector(".deleteBtn");
-    
-    if(target === "checkbox") {
-        task.classList.toggle("done", chbox.checked);
-    }
-
-    if(target === btn) {
-        taskContainer.remove();
-    }
-});
-
-clearBtn.addEventListener("click", () => {
-    tasks.textContent = "";
-});
+tasks.addEventListener("click", toggleTaskDoneOrRemove);
+clearBtn.addEventListener("click", () => tasks.textContent = "");
