c96392266ea16ac67a354a69eec2ad9070a65f55
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2026-03-17 20:25:25 +0100

    fix: correct playwright setup

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,18 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto('/');
+  await expect(page).toHaveTitle("To-Do's");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('add task', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: '✚' }).click();
+  await expect(page.getByRole('heading', { name: 'To-Do\'s 📜' })).toBeVisible();
+});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test('clear all tasks', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Clear All' }).click();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('input field', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('textbox').fill('Add your task');
 });
